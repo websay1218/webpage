89eeaad51c6411db92ac90bb4fe1fbf4a7976214
say <sage37@naver.com>
2021-01-13 23:52:44 +0900

    cocacola javascript code

--- 20210109_cocacola/src/js/common.js
+++ 20210109_cocacola/src/js/common.js
@@ -1,3 +1,29 @@
+const listSel = document.querySelectorAll('li.sel');
+const sec = document.querySelector('.sec');
+let idx = 0;
+const colorList = [
+    bgFirst ='#0062be',
+    bgSecound ='#e60c2c',
+    bgThird = '#1e1e1e'
+]
+
+console. log(listSel);
+
 function imgSlider(any) {
     document.querySelector('.pepsi').src = any;
 }
+
+listSel.forEach(function (list) {
+    list.addEventListener('click', function(event) {
+        const sellLi = event.target.parentNode;
+        imgSlider(event.target.src);
+
+        if (sellLi.classList.contains('sel00')) {
+            sec.style.backgroundColor = colorList[0];
+        } else if (sellLi.classList.contains('sel01')){
+            sec.style.backgroundColor = colorList[1];
+        } else {
+            sec.style.backgroundColor = colorList[2];
+        }
+    })
+})
