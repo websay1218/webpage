513c43e8bc4a947aa64a0867f302c42609fc74f1
say <sage37@naver.com>
2021-01-20 23:22:17 +0900

    clip-path & Scrath

---
+++ 20210120_Scratch/src/js/common.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousemove', (e) => {
+    const body =document.querySelector('body');
+    const bubbles = document.createElement('span');
+    bubbles.style.left = -75 + e.offsetX+'px';
+    bubbles.style.top = -75 + e.offsetY+'px';
+    body.appendChild(bubbles);
+})
